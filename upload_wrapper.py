341b6a5aad0ef451a1c6cc530bcb77dbc9405e86
SecurityResearcher <38038417+SecurityResearcher8@users.noreply.github.com>
2021-12-08 23:31:56 +0530

    Update upload_wrapper.py

--- upload_wrapper.py
+++ upload_wrapper.py
@@ -16,7 +16,7 @@ url = 'http://127.0.0.1:8000/api/v1/upload'
 
 #path = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']+'/workspace/'+file
 
-path = os.environ['JENKINS_HOME']+'/jobs/'+'/workspace/'+os.environ['JOB_NAME']+file
+path = os.environ['JENKINS_HOME']+'/jobs'+'/workspace/'+os.environ['JOB_NAME']+/file
 
 
 print(file)
