c0cf5202a007316295ccdccc016d5401a42fd065
SecurityResearcher <38038417+SecurityResearcher8@users.noreply.github.com>
2021-12-27 18:07:08 +0530

    Create upload_wrapper.py

--- upload_wrapper.py
+++ upload_wrapper.py
@@ -7,21 +7,22 @@ import requests
 file = sys.argv[2]
 Authorization = sys.argv[4]
 
-url = 'http://127.0.0.1:8000/api/v1/upload'
+url = 'http://127.0.0.1:8001/api/v1/upload'
 
 # url = 'http://0.0.0.0:8000/api/v1/upload'
 
-path = os.environ['JENKINS_HOME']+'/workspace/'+os.environ['JOB_NAME']+'/'+file
+# path = os.environ['JENKINS_HOME']+'/workspace/'+os.environ['JOB_NAME']+'/'+file
 
 # /Users/NowSecure/.jenkins/workspace/Test
 
 # FileNotFoundError: [Errno 2] No such file or directory: '/Users/NowSecure/.jenkins/jobs/Build/lastStable/workspace/app/diva-beta.apk'
 
 # /var/lib/jenkins/jobs/Build/lastStable/archive/app$ 
+# /var/lib/jenkins/workspace/Dynamic Analysis
 
 #path = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']/'+'/workspace/'+file
 
-#path = os.environ['JENKINS_HOME']+'/jobs/'+'/workspace/'+os.environ['JOB_NAME']/+file
+path = os.environ['JENKINS_HOME']+'/jobs/'+'/workspace/'+os.environ['JOB_NAME']/+file
 
 print(file)
 
@@ -31,6 +32,6 @@ headers = { "Authorization" : Authorization }
 
 response = requests.post(url,files=files,headers=headers)
 
-print(response.json())
+# print(response.json())
 
 #python wrapper.py -n App_Name.apk -Authorization <API_KEY>
