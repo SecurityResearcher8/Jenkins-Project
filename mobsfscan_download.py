30322dad6020051a8ab2723a7e34ff0fa993223d
SecurityResearcher <38038417+SecurityResearcher8@users.noreply.github.com>
2022-01-03 12:48:50 +0530

    Add files via upload

---
+++ mobsfscan_download.py
@@ -0,0 +1,35 @@
+from mobsfscan.mobsfscan import MobSFScan
+src = 'tests/assets/src/java/java_vuln.java'
+scanner = MobSFScan([src], json=True)
+scanner.scan()
+
+#opt2 = sys.argv[1]
+#appID = sys.argv[2]
+#opt3 = sys.argv[3]
+#token = sys.argv[4]
+
+#url = 'https://appsecure.accenture.com/api/executive_report/'
+
+#url = 'http://api/v1/download_pdf/'
+
+#print apk_file
+
+#path = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']+'/ws/'+name
+
+#print(path)
+
+#files = {'file': open(path,'rb')}
+
+#values = {
+		  'Uid' : appID,
+#           }
+		
+#authtoken = "JWT "+str(token)
+
+#headers = { "Authorization" : authtoken }
+
+#response = requests.post(url,data=values,headers=headers)
+
+#open("Report-file", "wb").write(response.content)
+    
+#python download.py -hash 96fcb6161620da89c82d3211df514848 -scan_type apk -Authorization 7e4fa6e935d190361b3a8b193c4051e1cb6f897c0a334121e0dfa1f1c3559251
